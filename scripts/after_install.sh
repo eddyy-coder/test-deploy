8b94d8e741274341e2a1569e0d0ef2508817bf75
Edwyn-Coder <edwyn.tellis@cintal.com>
2024-05-20 12:25:24 +0530

    dsf

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/test-deploy
+cd /home/ubuntu/test-deploy
 npm install
 sudo pm2 restart app.js
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get -y update
-cd /home/ec2-user/test-deploy
+cd /home/ubuntu/test-deploy
 git checkout main
 git pull
